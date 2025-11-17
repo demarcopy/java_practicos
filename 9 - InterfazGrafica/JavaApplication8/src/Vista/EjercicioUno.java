497a5824aae9cdece146700742bb82d33b4855ca
demarcopy <96490772+demarcopy@users.noreply.github.com>
2025-11-17 10:40:43 -0300

    .
    .

---
+++ 9 - InterfazGrafica/JavaApplication8/src/Vista/EjercicioUno.java
@@ -0,0 +1,13 @@
+package Vista;
+
+import Vista.VentanaUno;
+
+public class EjercicioUno {
+
+
+    public static void main(String[] args) {
+        VentanaUno ven = new VentanaUno();
+        ven.setVisible(true);
+    }
+    
+}
